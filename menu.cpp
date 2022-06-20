fd8a04b2a581bf64604e33b0c0834e534ea489a8
Jivko <jivkon1410@gmail.com>
2022-06-20 12:52:09 +0300

    updated menu - added sign in and sign up option

--- menu.cpp
+++ menu.cpp
@@ -3,11 +3,11 @@
 
 using namespace std;
 
-void login_menu()
+void home_menu()
 {
     cout << 
     "+----------------------------------------+\n"\
-    "|               Login menu               |\n"\
+    "|               Home menu                |\n"\
     "| 1.Sign in                              |\n"\
     "| 2.Sign up                              |\n"\
     "| 3.Exit                                 |\n"\
@@ -20,7 +20,60 @@ void login_menu()
     "+----------------------------------------+\n";
 }
 
-void print_menu()
+void signin_menu()
 {
+    do
+    {
+        system("cls");
+        
+        cout << "|               Sign in                   |\n";
+        string username, password;
+        cout << "Enter username: ";
+        cin >> username;
+        cout << "Enter password";
+        cin >> password;
+    } while(!user_exists(username, password));
 
+    return user_id(username);
+}
+
+string signup_menu()
+{
+    do
+    {
+        system("cls");
+        
+        cout << "|               Sign up                   |\n";
+        string username, password;
+        cout << "Enter username: ";
+        cin >> username;
+        cout << "Enter password";
+        cin >> password;
+    } while(username_exists(username));
+    return add_user(username, password); // returns id
+}
+
+string print_menu()
+{
+    system("cls");
+    uint8_t input = -1;
+
+    home_menu();
+    cin >> input;
+
+    switch(input)
+    {
+        case 1:
+            signin_menu();
+            break;
+        case 2:
+            signup_menu();
+            break;
+        case 3:
+            system("cls");
+            cout << "Exited from the bank";
+            return "";
+    }
+    system("cls");
+    return ;
 }
