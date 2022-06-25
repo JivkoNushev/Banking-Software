1e63534602e8a4a06ad714712dfaa37302fca30d
Yoan Laptop <yoanivanov9@gmail.com>
2022-06-25 23:24:10 +0300

    sha256 not working :(

--- hash.cpp
+++ hash.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <string>
-//#include <stdint-gcc.h>
+#include <stdint-gcc.h>
 #include <string.h>
-//#include <byteswap.h>
+#include <byteswap.h>
 #include "hash.h"
 using namespace std;
 
@@ -14,7 +14,7 @@ void print_binary_number(uint8_t n)
             cout << 1;
         else
             cout << 0;
-        n>>=1;
+        n >>= 1;
     }
     cout << "-";
 }
@@ -26,7 +26,7 @@ void print_binary_number(uint64_t n)
             cout << 1;
         else
             cout << 0;
-        n>>=1;
+        n >>= 1;
     }
     cout << "-";
 }
@@ -40,7 +40,7 @@ void print(uint8_t *input, int size)
     cout << endl;
 }
 
-//TODO: little and big endian
+// TODO: little and big endian
 int alo()
 {
     char a[] = "abc";
@@ -49,32 +49,51 @@ int alo()
     uint64_t length = strlen(input);
     int curr_block_length = 0;
 
+    print_binary_number(length * 8);
+    __builtin_bswap64(length);
+
+    print_binary_number(length * 8);
     // 512 bits(64 bytes) blocks
     // 448 bits(56 bytes) of info and 64(8 bytes) for length of message
     cout << "Lenth: " << length << endl;
     if (length < 56)
     {
-        uint8_t block[64] = {0};
-        for (int i = 0; i < length; i++)
-            block[i] = input[i];
+        char a[] = "abc";
+        char *input = a;
 
-        curr_block_length += length;
-        print(block, curr_block_length);
+        uint64_t length = strlen(input);
+        int curr_block_length = 0;
 
-        // add 1 and padd with 0s until 56 bytes
-        block[curr_block_length] = 128;
-        curr_block_length++;
-        print(block, curr_block_length);
+        // 512 bits(64 bytes) blocks
+        // 448 bits(56 bytes) of info and 64(8 bytes) for length of message
 
-        curr_block_length = 56;
-        print(block, curr_block_length);
+        // cout << "Lenth: " << length << endl;
+        if (length < 56)
+        {
+            uint8_t block[64] = {0};
+            for (int i = 0; i < length; i++)
+            {
+                block[i] = input[i];
+            }
 
-        // after that add 8 bytes of length of bits
+            curr_block_length += length;
+            // cout << "ASCII to binary: " << endl;
+            print(block, curr_block_length);
 
-        block[curr_block_length] = length * 8;
-        curr_block_length = 64;
-        print_binary_number(length * 8);
-        // print(block, curr_block_length);
+            // cout << "Add 1 between msg and padded 0s: " << endl;
+            block[curr_block_length] = 128;
+            curr_block_length++;
+            print(block, curr_block_length);
+
+            // cout << "Padd with 0s until 56 bytes: " << endl;
+            curr_block_length = 56;
+            print(block, curr_block_length);
+
+            // cout << "after that add 8 bytes of length of bits: << endl;
+            block[curr_block_length] = length * 8;
+            curr_block_length = 64;
+            print(block, curr_block_length);
+        }
     }
 }
 
