92727657e61aba0581948ddcec0093c6bc0d6c07
Jivko <jivkon1410@gmail.com>
2022-06-26 15:08:47 +0300

    added main file

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "user.h"
+#include "bill.h"
+#include "transaction.h"
+#include "menu.h"
+
+using namespace std;
+
+/////// main isnt in github
+
+int main(int argc, char** argv)
+{
+    while(1)
+    {
+        int userId = print_home_menu();
+        if(userId == -1) return 0;
+
+        string userName = User::user_userName(userId), password = User::user_password(userId);
+
+        User logged_user(userName, password, userId);
+
+        print_menu(logged_user);
+    }
+
+    return 0;
+}
